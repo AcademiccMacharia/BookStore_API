25f789b49bcd9a5226cac235c80563b01c5ac33c
AcademiccMacharia <machariab19@gmail.com>
2023-06-20 09:55:09 +0300

    adding authorization using jwt

--- src/controllers/memberController.js
+++ src/controllers/memberController.js
@@ -1,69 +1,69 @@
 const config = require('../config/bookStoreConfig');
 const mssql = require('mssql');
 const bcrypt = require('bcrypt');
+const { getMemberByID } = require('../utils/getMember');
+const { tokenGenerator } = require('../utils/tokens');
 
 module.exports = {
-    postMember: async (req, res) => {
-        try {
-            let member = req.body;
+  postMember: async (req, res) => {
+    try {
+      let member = req.body;
+      let hashedPwd = await bcrypt.hash(member.Password, 8);
 
-            let hashedPwd = await bcrypt.hash(member.Password, 8);
+      let sql = await mssql.connect(config);
+      if (sql.connected) {
+        let results = await sql.request()
+          .input("Name", member.Name)
+          .input("Address", member.Address)
+          .input("ContactNumber", member.ContactNumber)
+          .input("Password", hashedPwd)
+          .execute("library.create_member");
 
-            let sql = await mssql.connect(config);
-            if (sql.connected) {
-                let results = await sql.request()
-                    .input("Name", member.Name)
-                    .input("Address", member.Address)
-                    .input("ContactNumber", member.ContactNumber)
-                    .input("Password", hashedPwd)
-                    .execute("create_member");
+        console.log(results);
+        res.json({
+          success: true,
+          message: "Member created successfully",
+          results: results.recordsets[0]
+        });
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  },
 
-                console.log(results);
-                res.json(results);
-            }
-        } catch (error) {
-            console.error("Error:", error);
-            res.status(500).json({ error: "Internal server error" });
+  loginMember: async (req, res) => {
+    let { MemberID, Password } = req.body;
+    try {
+      let member = await getMemberByID(MemberID);
+      if (member) {
+        let passwordMatch = await bcrypt.compare(Password, member.Password);
+        if (passwordMatch) {
+          let token = await tokenGenerator({
+            MemberID: member.MemberID,
+            roles: "admin"
+          })
+
+          res.json({
+            success: true,
+            message: "Logged in Successfully",
+            token
+          });
+        } else {
+          res.status(401).json({
+            success: false,
+            message: "Wrong credentials"
+          });
         }
-    },
-
-    loginMember: async (req, res) => {
-        try {
-          let { MemberID, Password } = req.body;
-      
-          let sql = await mssql.connect(config);
-          if (sql.connected) {
-            let results = await sql
-              .request()
-              .input("MemberID", MemberID)
-              .execute("GetMemberByID");
-      
-            let member = results.recordset[0];
-            if (member) {
-              let passwordMatch = await bcrypt.compare(Password, member.Password);
-              passwordMatch? res.json({
-                    success: true,
-                    message: "Logged in Successfully"
-                  })
-                : res.status(401).json({
-                    success: false,
-                    message: "Wrong credentials"
-                  });
-            } else {
-              res.status(404).json({
-                success: false,
-                message: "No user found"
-              });
-            }
-          } else {
-            res.status(500).json({
-              success: false,
-              message: "Internal server error"
-            });
-          }
-        } catch (error) {
-          console.error("Error:", error);
-          res.status(500).json({ error: "Internal server error" });
-        }
-      }      
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "No user found"
+        });
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
 };
