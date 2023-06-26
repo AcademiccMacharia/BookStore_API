f34c6153b1da47b4a8cf6cc7f5944f2c36fcc647
Mumbi njuguna <mumbijoynjuguna@gmail.com>
2023-06-26 15:07:00 +0300

    admin for the bookstore api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,65 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+
+import Members from './components/admin/members';
+
+
+import Books from './components/admin/books';
+import Loans from './components/admin/loans';
+import SideBar from './components/admin/sideBar';
+
+
+const router = createBrowserRouter(
+  [
+    {
+       
+            path: '/',
+            title: 'HOME',
+            element:<SideBar/>,
+            children:[
+    
+          
+          {
+            path: '/books',
+           title:'BOOKS',
+           element:<Books/>,
+            cName:'nav-text'
+          },
+          {
+            path: '/users',
+            title:'USERS',
+            element:<Members/>,
+            cName:'nav-text'
+           
+          },
+          {
+            path: '/loans',
+            title:'LOANS',
+            element:<Loans/>,
+          
+            cName:'nav-text'
+          },
+          { 
+            path: '/settings',
+            title:'SETTINGS',
+            element:<Members/>,
+           cName:'nav-text'
+          } 
+        ]}
+             
+]
+ );
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    
+    <RouterProvider router={router} />
+  
+   
+   );
 }
 
 export default App;
--- client/src/components/admin/adminSettings.js
+++ client/src/components/admin/adminSettings.js
@@ -1,9 +1,47 @@
-import React from 'react'
+// import React, { useState } from "react";
+// import "./sidecomponent.css";
+// import * as AiIcons from "react-icons/ai";
+// import book from "./images/book.png";
+// import profile from "./images/profile.png";
+// import { Link } from "react-router-dom";
+// import { SidebarData } from "./sideBar";
+// import { IconContext } from "react-icons/lib";
+// import SingleBook from "./singleBook";
 
-const settings = () => {
-  return (
-    <div>settings</div>
-  )
-}
+// const AdminSettings = () => {
+//   const [sidebar, setSidebar] = useState(false);
 
-export default settings
+//   const showsidebar = () => setSidebar(!sidebar);
+
+//   return (
+//     <>
+//       <IconContext.Provider value={{ color: "white" }}>
+//         <div className="admin-dashboard">
+//           <h4>Admin dashboard</h4>
+//           <div className="input">
+//             <button>
+//               <AiIcons.AiOutlineSearch />
+//             </button>
+//             <input type="text" placeholder="search something" />
+//           </div>
+//         </div>
+
+//         <div className="All-books">
+//           <SingleBook />
+//           <SingleBook />
+//           <SingleBook />
+//           <SingleBook />
+//           <SingleBook />
+//         </div>
+//         {/* <div className="navbar ">
+ 
+//      <Link to='/' className= "menu-bars">
+//      < FaIcons.FaHamburger onClick={showsidebar}/>
+//      </Link>
+//     </div> */
+
+//     </>
+//   );
+// }
+
+// export default AdminSettings;
