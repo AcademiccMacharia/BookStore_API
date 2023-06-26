ece4cc52e4acffabb6a24f78be9cc987dfaf2174
Mumbi njuguna <mumbijoynjuguna@gmail.com>
2023-06-26 23:04:07 +0300

    loanedbooks in admin done

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,71 @@ import Header from './components/user/header';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Return from './components/user/return';
 import Loans from './components/user/loans';
+import Members from './components/admin/members';
 
 
-const router = createBrowserRouter([
+import Books from './components/admin/books';
+import LoanedBooks from './components/admin/loans';
+import SideBar from './components/admin/sideBar';
+import Addbook from './components/admin/addbook';
+import Deletebook from './components/admin/deletebook';
+
+import Login from './components/login/Login';
+import Signup from './components/signup/signup';
+
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Signup />,
+    },
+    {
+       
+      path: '/',
+      title: 'HOME',
+      element:<SideBar/>,
+      children:[
+
+    
+    {
+      path: '/books',
+     title:'BOOKS',
+     element:<Books/>,
+      cName:'nav-text'
+    },
+    {
+      path: '/users',
+      title:'USERS',
+      element:<Members/>,
+      cName:'nav-text'
+     
+    },
+    {
+      path: '/loanedbooks',
+      title:'LOANS',
+      element:<LoanedBooks/>,
+    
+      cName:'nav-text'
+    },
+    { 
+      path: '/addbook',
+      title:'Addbook',
+      element:<Addbook/>,
+     cName:'nav-text'
+    },
+    { 
+      path: '/deletebook',
+      title:'Addbook',
+      element:<Deletebook/>,
+     cName:'nav-text'
+    } 
+  ]},
   {
     path: '/',
     element: <Panel />, 
@@ -31,87 +93,12 @@ const router = createBrowserRouter([
       }
     ]
   }
-]);
-
-
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import Members from './components/admin/members';
-
-
-import Books from './components/admin/books';
-import Loans from './components/admin/loans';
-import SideBar from './components/admin/sideBar';
-import Addbook from './components/admin/addbook';
-import Deletebook from './components/admin/deletebook';
-
-
-const router = createBrowserRouter(
-  [
-    {
-       
-            path: '/',
-            title: 'HOME',
-            element:<SideBar/>,
-            children:[
-    
-          
-          {
-            path: '/books',
-           title:'BOOKS',
-           element:<Books/>,
-            cName:'nav-text'
-          },
-          {
-            path: '/users',
-            title:'USERS',
-            element:<Members/>,
-            cName:'nav-text'
-           
-          },
-          {
-            path: '/loans',
-            title:'LOANS',
-            element:<Loans/>,
-          
-            cName:'nav-text'
-          },
-          { 
-            path: '/addbook',
-            title:'Addbook',
-            element:<Addbook/>,
-           cName:'nav-text'
-          },
-          { 
-            path: '/deletebook',
-            title:'Addbook',
-            element:<Deletebook/>,
-           cName:'nav-text'
-          } 
-        ]}
-             
-]
- );
-
-
-
-function App() {
-
-  const router = createBrowserRouter([
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Signup />,
-    }
+  
   ]);
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
